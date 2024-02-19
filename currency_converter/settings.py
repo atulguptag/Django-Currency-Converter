d6e226e814088ec588ac7334ffc542f4993999ea
Atul Gupta <atulguptag111@gmail.com>
2024-02-19 13:19:08 +0530

    Change debug to true and added ".vercel.com" in allowed hosts

--- currency_converter/settings.py
+++ currency_converter/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s8u^s8=qcbv@6$)1hwjff8qi)=wri5o6_0y&5&$wtxk-+!owud'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.com']
 
 
 # Application definition
